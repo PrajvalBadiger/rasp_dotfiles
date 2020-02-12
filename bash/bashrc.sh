f339ac3c317b6051f53ae72b7b7b9eba92fb9a45
Prajval <er.prajvalbadiger@gmail.com>
2020-02-12 16:12:50 +0530

    bashrc added to the repo with minimum setting

---
+++ bash/bashrc.sh
@@ -0,0 +1,27 @@
+export PATH=/home/pi/.local/bin/autopep8:$PATH
+source ~/.bash_prompt
+set -o vi
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+force_color_prompt=yes
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
